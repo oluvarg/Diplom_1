c46dc18b725b43ebf7f618ec24d4727f60a001fc
oleuva <oleg.uvarov@sitronics-kt.ru>
2024-05-17 14:42:43 +0300

    diplom_1_1

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from unittest.mock import Mock
+from data import Data
+
+
+@pytest.fixture(scope='function')
+def mock_bun():
+    mock_bun = Mock()
+    mock_bun.get_name.return_value = Data.BUN_NAME[0]
+    mock_bun.get_price.return_value = Data.BUN_PRICE[0]
+    return mock_bun
+
+
+@pytest.fixture(scope='function')
+def mock_ingredient():
+    mock_ingredient = Mock()
+    mock_ingredient.get_name.return_value = Data.ING_NAME
+    mock_ingredient.get_type.return_value = Data.ING_TYPE
+    mock_ingredient.get_price.return_value = Data.BUN_PRICE
+    return mock_ingredient
